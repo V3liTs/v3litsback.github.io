66aeeaa461f2e656e6acd96c1aad01503e1f3bf3
V3liTs <v3lits@gmail.com>
2023-02-15 14:29:31 +0100

    lezione 17

---
+++ example-app/resources/views/test.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>TESTTTTTTTT</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+
+</head>
+
+<body class="antialiased">
+    <p>Questa è la mia pagina di test</p>
+    <a href="<?php echo url('/ciao'); ?>">Vai alla pagina ciao</a> |
+    <a href="<?php echo url('/'); ?>">Vai alla home</a>
+    <a href="<?php echo route('profile'); ?>">Vai a profile</a>
+    <a href="<?php echo url('/user/profile'); ?>">Vai sempre al profile</a>
+    <a href="<?php echo route('user', ['id'=> 81]); ?>">Vai a user con param</a>
+</body>
+
+</html>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MyTestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,40 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/testmio', 'test');
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/user/profile', function () {
+    return "Utente";
+})->name('profile');
+
+Route::get('/ciao', function () {
+    return "Hello World questo è scritto a video";
+});
+
+Route::view('/provaparametro', 'prova', ['name' => 'Paolo']);
+
+Route::redirect("/ciaomondo", "/ciao");
+
+Route::get('/user/{id}', function (string $id) {
+    return 'Il tuo id user è '.$id;
+})->name("user");
+
+Route::get('/username/{name}', function (string $name) {
+    return 'Questo username è valido : '.$name;
+})->where('name', '[A-Za-z]+');
+
+Route::get('/users/{name}/{number}', function ($name, $number) {
+    return 'Questo username è valido : '.$name . " num ".$number;
+})->where(['name' => '[A-Za-z]+','number' => '[0-9]+']);
+
+
+Route::get('/posts/{post}/comments/{comment}', function (string $mario, string $commentId) {
+    return 'Il tuo id post è '.$mario. " il commento id è ".$commentId;
+});
+
+Route::get('/hello-controller/', [MyTestController::class,'greeting']);
